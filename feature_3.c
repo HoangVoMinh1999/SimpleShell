f2bf8490592b709e0d928d18f29ef943551b279a
HoangVoMinh1999 <hoangvm.hiephoa@gmail.com>
2019-09-28 14:43:15 +0700

    can't get data from file

--- feature_3.c
+++ feature_3.c
@@ -3,7 +3,7 @@
 #include <string.h> 
 
 #define MAX_CMD_LEN  128
-char data[100]={0};
+int data[100];
 /* ---FUNCTION--- */
 int Check_File(char str[])
 {
@@ -16,7 +16,7 @@ int Check_File(char str[])
 		}
 		i++;
 	}
-	if (count==2)
+	if (count==3)
 	{
 		return 1;
 	}
@@ -30,7 +30,39 @@ int Check_Sign(char str[])
 	}
 	return 1;
 }
-
+void Getter_Data_From_File(char *sign,char *file_name,char data[])
+{
+	printf("Da chay ham nay\n");
+	int len_of_data=0;
+	if (sign==60) // dấu hiệu input
+	{
+		FILE *f=fopen(file_name,"rt");
+		if (f==NULL)
+		{
+			printf("No file for getting data");
+			return NULL;
+		}
+		fscanf(f,"%d",&len_of_data);
+		printf("da chay cho nay\n");
+		printf("%d\n",len_of_data);
+		for (int i=0;i<len_of_data;i++)
+		{
+			fscanf(f,"%d",&data[i]);
+		}
+		fclose(f);
+	}
+	else if (sign==61)
+	{
+		FILE *f=fopen(file_name,"r");
+		if (f==NULL)
+		{
+			printf("No file for getting data");
+			return NULL;
+		}
+		fscanf(f,"%d",data);
+		fclose(f);
+	}
+}
 /* ---MAIN--- */
 int main() 
 { 
@@ -39,25 +71,36 @@ int main()
 	{
 		printf("osh>");
 		fgets(command,MAX_CMD_LEN, stdin);
-		printf("%s\n",command);
-		if (Check_File(command)==1)
+		strncat(command," ",sizeof(char));
+		if (Check_File(command)==1) // Kiểm tra dòng code hợp lệ vs file
 		{
 			/* ---get function--- */
 			char *token=strtok(command," "); 
-			char *Redirect_File[3];
+			char *Redirect_File[4];
 			Redirect_File[0]=token; // Redirect_File[0]=function
-			printf("%s\n",Redirect_File[0]);
 			/* ---get sign and file name--- */
 			int i=1;
-			while (token != NULL && i!=4)
+			while (token != NULL)
 			{
 				token=strtok(NULL, " ");
 				Redirect_File[i]=token; // Redirect_File[1]=sign,Redirect_File[2]=file_name
-				printf("%s\n",Redirect_File[i]);
 				i++;
 			}
+			if(Check_Sign(Redirect_File[1])==0)// nhập sai kí tự , khác < >
+			{
+				printf("Wrong command\n");
+				continue;
+			}
+			else
+			{
+				Getter_Data_From_File(Redirect_File[1],Redirect_File[2],data);
+				for (int i=0;i<10;i++)
+				{
+					printf("%d ",data[i]);
+				}
+			}
+			
 		}
 	}
-
 	return 0; 
 } 
