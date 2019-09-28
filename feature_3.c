1532374eb664fb7917a7162ce714875601633ff5
HoangVoMinh1999 <hoangvm.hiephoa@gmail.com>
2019-09-28 21:14:33 +0700

    can read and create file;can't get data from file

--- feature_3.c
+++ feature_3.c
@@ -1,11 +1,12 @@
 #include "unistd.h"
 #include <stdio.h> 
 #include <string.h> 
-
+#include <stdlib.h>
 #define MAX_CMD_LEN  128
 int data[100];
+int len_of_data;
 /* ---FUNCTION--- */
-int Check_File(char str[])
+int Check_Command(char str[])
 {
 	int i=0,count=0;
 	while (str[i]!='\0')
@@ -30,36 +31,40 @@ int Check_Sign(char str[])
 	}
 	return 1;
 }
-void Getter_Data_From_File(char *sign,char *file_name,char data[])
+void Getter_Data_From_File(char *sign,char *file_name,int len_of_data,int data[])
 {
-	printf("Da chay ham nay\n");
-	int len_of_data=0;
-	if (sign==60) // dấu hiệu input
+	if (sign[0]==60) // dấu hiệu input
 	{
-		FILE *f=fopen(file_name,"rt");
-		if (f==NULL)
-		{
-			printf("No file for getting data");
-			return NULL;
-		}
-		fscanf(f,"%d",&len_of_data);
-		printf("da chay cho nay\n");
-		printf("%d\n",len_of_data);
-		for (int i=0;i<len_of_data;i++)
-		{
-			fscanf(f,"%d",&data[i]);
-		}
-		fclose(f);
-	}
-	else if (sign==61)
-	{
+		printf("Da chay ham nay\n");
 		FILE *f=fopen(file_name,"r");
 		if (f==NULL)
 		{
-			printf("No file for getting data");
+			printf("FILE NOT EXIST");
 			return NULL;
 		}
-		fscanf(f,"%d",data);
+		int i=0;
+		fscanf(f,"%d",len_of_data);
+		printf("%d\n",len_of_data);
+		for (int i=0;i<len_of_data;i++)
+		{
+			fscanf(f,"%d",data[i]);
+			printf("%d",data[i]);
+		}
+		fclose(f);
+	}
+	else if (sign[0]==62)
+	{
+		FILE *f=fopen(file_name,"w");
+		if (f==NULL)
+		{
+			printf("No file output !!!");
+			return NULL;
+		}
+		fprintf(f,"%d",len_of_data);
+		for (int i=0;i<len_of_data;i++)
+		{
+			fprintf(f,"%s",data[i]);
+		}
 		fclose(f);
 	}
 }
@@ -71,8 +76,10 @@ int main()
 	{
 		printf("osh>");
 		fgets(command,MAX_CMD_LEN, stdin);
-		strncat(command," ",sizeof(char));
-		if (Check_File(command)==1) // Kiểm tra dòng code hợp lệ vs file
+		command[strlen(command)-1]=' ';
+		command[strlen(command)]='\0';
+		printf("%saa\n",command);
+		if (Check_Command(command)==1) // Kiểm tra dòng code hợp lệ vs file
 		{
 			/* ---get function--- */
 			char *token=strtok(command," "); 
@@ -80,11 +87,15 @@ int main()
 			Redirect_File[0]=token; // Redirect_File[0]=function
 			/* ---get sign and file name--- */
 			int i=1;
-			while (token != NULL)
+			while (token != NULL  && i<3)
 			{
 				token=strtok(NULL, " ");
 				Redirect_File[i]=token; // Redirect_File[1]=sign,Redirect_File[2]=file_name
 				i++;
+			}
+			for (int i=0;i<4;i++)
+			{
+				printf("vi tri thu [%d]=%saa\n",i,Redirect_File[i]);
 			}
 			if(Check_Sign(Redirect_File[1])==0)// nhập sai kí tự , khác < >
 			{
@@ -93,7 +104,7 @@ int main()
 			}
 			else
 			{
-				Getter_Data_From_File(Redirect_File[1],Redirect_File[2],data);
+				Getter_Data_From_File(Redirect_File[1],Redirect_File[2],len_of_data,data);
 				for (int i=0;i<10;i++)
 				{
 					printf("%d ",data[i]);
